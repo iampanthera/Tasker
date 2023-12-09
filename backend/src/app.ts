1ef2f2ce842d242a037422fba9ac2b0f1c6a51ec
Mian G <miantalhaakram@gmail.com>
2023-12-08 19:03:13 -0500

    BE adjustments

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,24 +1,59 @@
-import express from 'express';
+import express, { Application, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+import mongoSanitize from 'express-mongo-sanitize';
 import routes from './routes';
 import { PORT, MONGO_URI } from './utils/secrets';
 
-const app = express();
+class App {
+  private app: Application;
 
-// Connect to MongoDB
-mongoose
-  .connect(MONGO_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error(err));
+  constructor() {
+    this.app = express();
+    this.config();
+    this.connectToDatabase();
+    this.middleware();
+    this.routes();
+    this.listen();
+  }
 
-// Middleware
-app.use(cors());
-app.use(express.json());
+  private config(): void {
+    this.app.use(helmet());
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
 
-// app.use('/api', (req, res) => res.send('Hello World'));
+  private connectToDatabase(): void {
+    mongoose
+      .connect(MONGO_URI)
+      .then(() => console.log('Connected to MongoDB'))
+      .catch((err) => console.error(err));
+  }
 
-// Routes
-app.use('/api', routes);
+  private middleware(): void {
+    this.app.use(mongoSanitize());
+    this.app.use(morgan('combined'));
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+    const limiter = rateLimit({
+      windowMs: 15 * 60 * 1000,
+      max: 100,
+    });
+    this.app.use('/api', limiter);
+  }
+
+  private routes(): void {
+    this.app.use('/api', routes);
+  }
+
+  private listen(): void {
+    this.app.listen(PORT, () =>
+      console.log(`Server listening on port ${PORT}`)
+    );
+  }
+}
+
+new App();
